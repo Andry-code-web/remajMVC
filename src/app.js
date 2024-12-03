d8288fa2424ad16158d34bccff966d2dd41e3cc0
mou <allan1moises@gmail.com>
2024-12-03 10:20:29 -0500

    para_borrar

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/auth', require('./routes/auth.routes'));
 app.use('/auctions', require('./routes/auction.routes'));
 app.use('/btn_menu', require('./routes/btn_menu.routes'));
+
 // Socket.IO
 io.on('connection', (socket) => {
   socket.on('join-auction', (auctionId) => {
--- src/routes/btn_menu.routes.js
+++ src/routes/btn_menu.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const btn_menuController = require('../controllers/btn_menu.controller');
 
-router.get('/index',btn_menuController.getAllbtn_menu);
+router.get('/index', btn_menuController.getAllBtn_menu);
 
 module.exports = router;

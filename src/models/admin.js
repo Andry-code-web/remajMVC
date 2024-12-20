196774c55fdae8de51e3bfec4b8983d33e8820a4
jesusdorian999019 <ururiandia@gmail.com>
2024-12-20 11:28:58 -0500

    V1 añadimos videos en home y chat ejs cambios

--- src/models/admin.js
+++ src/models/admin.js
@@ -1,4 +1,3 @@
-
 const db = require('../config/database');
 const bcrypt = require('bcryptjs');
 
--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -79,6 +79,7 @@ const getImagenesInmuebles = async () => {
 const createRemate = async (datosRemate) => {
     const query = `
         INSERT INTO remates 
+        
         (ubicacion, precios, descripcion, categoria, N_banos, N_habitacion, pisina, patio, cocina, cochera, 
          balcon, jardin, pisos, comedor, sala_start, studio, lavanderia, fecha_remate, hora_remate, estado, tamaño_propiedad, usuario_admin_id)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)

0c70b0b2940c927357bfbd346081902bdd660426
Andry-code-web <82222276+Andry-code-web@users.noreply.github.com>
2024-12-30 11:49:05 -0500

    cooke_false

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,
       maxAge: 3600000,
     },
   })

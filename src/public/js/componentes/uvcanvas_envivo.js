fbd4d98d8935e2921069f1428eb386ed7b5669c9
jeffjejeje <jeffersonarcemaquito@gmail.com>
2025-01-24 13:40:05 -0500

    estilos_chat_remtes_tags_uvcanvas

--- src/public/js/componentes/uvcanvas_envivo.js
+++ src/public/js/componentes/uvcanvas_envivo.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function animate() {
       ctx.clearRect(0, 0, width, height);
-      ctx.strokeStyle = 'rgba(44, 42, 42, 0.9)';
+      ctx.strokeStyle = 'rgba(104, 2, 2, 0.47)';
       ctx.lineWidth = 1;
 
       lines.forEach(line => {

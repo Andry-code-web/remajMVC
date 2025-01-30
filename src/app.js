b6484f406da07d64205622fb4509fe1522d83186
Andry-code-web <73274663@certus.edu.pe>
2025-01-30 17:16:26 -0500

    Add MySQL session store configuration for improved session management

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const socketIO = require('socket.io');
 const morgan = require('morgan');
 const flash = require('connect-flash');
 const { setUserLocals } = require('./middleware/auth.middleware');
+const MySQLStore = require('express-mysql-session')(session);
 const db = require('./config/database'); // Usando la conexión pool
 require('dotenv').config();
 
@@ -15,6 +16,15 @@ const server = http.createServer(app);
 const io = socketIO(server, {
   connectionStateRecovery: {},
 });
+
+const options ={
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'remajud',
+}
+
+const sessionStore = new MySQLStore(options);
 
 // Middleware
 app.use(cookieParser());
@@ -25,10 +35,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     secret: process.env.SESSION_SECRET || 'secret',
+    store: sessionStore,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,
       maxAge: 3600000,
     },
   })
